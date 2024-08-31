d0877ca9b552b6cb1b4485b8b3cdd3f4d6d64d8a
Александр Шестаков <dddd30065@gmail.com>
2024-08-31 23:31:32 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        byte b1 = 12;
+        short s1 = 234;
+        int i1 = 400505;
+        long l1 = 5006954004030L;
+        char ch1 = 'd';
+        boolean bool = false;
+        int a = 20;
+        int b = 15;
+        int sum = a+b;
+        int dif = a-b;
+        int multiplication = a*b;
+        int div = a/b;
+        int remains = a%b;
+        System.out.println("sum = " + sum);
+        System.out.println("dif = " + dif);
+        System.out.println("multiplication = " + multiplication);
+        System.out.println("div = " + div);
+        System.out.println("remains = " + remains);
+    }
+}
